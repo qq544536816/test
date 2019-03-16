6d6656797ff0e8c8bf3e247993b3fa961ab70c55
qq544536816 <544536816@qq.com>
2019-03-16 15:00:06 +0800

    test commit

---
+++ src/test/java/com/example/hellospringcloud/HellospringcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hellospringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellospringcloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
